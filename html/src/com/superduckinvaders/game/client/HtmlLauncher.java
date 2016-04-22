25921da9b9153d19c70698a097a07a717b748a1c
jameseh96 <00jamesh@gmail.com>
2016-04-22 18:16:57 +0100

    permitting resize

--- html/src/com/superduckinvaders/game/client/HtmlLauncher.java
+++ html/src/com/superduckinvaders/game/client/HtmlLauncher.java
@@ -1,19 +1,34 @@
 package com.superduckinvaders.game.client;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.superduckinvaders.game.DuckGame;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(1280, 720);
-        }
+    @Override
+    public void onModuleLoad () {
+        super.onModuleLoad();
+        com.google.gwt.user.client.Window.addResizeHandler(new ResizeHandler() {
+            public void onResize(ResizeEvent ev) {
+                Gdx.graphics.setWindowedMode(ev.getWidth(),ev.getHeight());
+            }
+        });
+    }
 
-        @Override
-        public ApplicationListener createApplicationListener () {
+    @Override
+    public GwtApplicationConfiguration getConfig () {
+        int height = com.google.gwt.user.client.Window.getClientHeight();
+        int width = com.google.gwt.user.client.Window.getClientWidth();
+        return new GwtApplicationConfiguration(width, height);
+    }
+
+    @Override
+    public ApplicationListener createApplicationListener () {
                 return new DuckGame();
         }
 }
